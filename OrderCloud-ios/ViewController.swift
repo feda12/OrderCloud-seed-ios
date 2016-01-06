32f0873bbd478dcb32bd424debadb86b71ccd3c5
Ben Le Cam <blecam@four51.com>
2016-01-06 08:58:24 -0600

    Initial Commit

---
+++ OrderCloud-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OrderCloud-ios
+//
+//  Created by Ben Le Cam on 1/6/16.
+//  Copyright © 2016 Ben Le Cam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
